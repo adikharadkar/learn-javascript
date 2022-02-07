e6a66d3ad8cf51ceb9b744a3d613daa6c482130e
adikharadkar <adityakharadkar27@gmail.com>
2022-02-07 19:16:20 +0530

    Update convertString.js

--- convertString.js
+++ convertString.js
@@ -3,8 +3,16 @@
 let num = "21";
 let dec = "30.5";
 
+// String to Integer
 console.log(parseInt(num));
+
+// String to Float
 console.log(parseFloat(dec));
+
 console.log(typeof parseInt(num));
 console.log(typeof parseFloat(dec));
 console.log(num, dec);
+
+// Integer to String
+let number = 44;
+console.log(number.toString());
