c964e94c7df96d9dc592350b4549363611c37c57
adikharadkar <adityakharadkar27@gmail.com>
2022-02-07 19:09:37 +0530

    Create convertString.js

---
+++ convertString.js
@@ -0,0 +1,10 @@
+// Program to convert a string into int and float
+
+let num = "21";
+let dec = "30.5";
+
+console.log(parseInt(num));
+console.log(parseFloat(dec));
+console.log(typeof parseInt(num));
+console.log(typeof parseFloat(dec));
+console.log(num, dec);
