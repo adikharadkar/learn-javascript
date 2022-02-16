66ed410a377e97e23ebcbb1f3f3dfcf3569d6e34
adikharadkar <adityakharadkar27@gmail.com>
2022-02-16 20:12:16 +0530

    Update fresco.js

--- fresco.js
+++ fresco.js
@@ -3,3 +3,19 @@ let myText = document.createTextNode("Text added by JS");
 let body = document.querySelector("body");
 myParagraph.appendChild(myText);
 body.appendChild(myParagraph);
+
+// END
+
+// START
+const str1 = "This is";
+const str2 = "Javascript";
+const str3 = str1 + str2;
+console.log(str3.length);
+// END
+
+// START
+const month = "January";
+const year = 2016;
+const date = 1;
+const whole = `0${date}-${month}-${year}`;
+console.log(whole);
