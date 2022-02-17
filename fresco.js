a87456215974ac294d73e160e61d5228c10c442c
adikharadkar <adityakharadkar27@gmail.com>
2022-02-17 14:45:07 +0530

    Update fresco.js

--- fresco.js
+++ fresco.js
@@ -19,3 +19,10 @@ const year = 2016;
 const date = 1;
 const whole = `0${date}-${month}-${year}`;
 console.log(whole);
+// END
+
+// START
+let input = prompt("Enter a value to convert from celcius to Fahrenheit");
+let output = (9 * parseInt(input)) / 5 + 32;
+console.log(parseFloat(output));
+// END
