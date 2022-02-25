7164df410a476249c226f3f56cd9a22c8e4791e5
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 18:31:03 +0530

    Update consoleDir.js

---
+++ consoleDir.js
@@ -0,0 +1,6 @@
+// The console.dir() method displays an interactive list of all the properties of a specified object.
+// It is presented as hierarchical listing of methods and properties of an object with closure triangles.
+// Simply console.dir() let us see all the properties of a specified object.
+
+const arr = ["Audi", "Mercedes", "BMW", "Lamborghini", "Ferrari", "Tesla"];
+console.dir(arr);
