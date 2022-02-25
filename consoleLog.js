954e3b2af475dd70a687beb4f57f553f3f82e211
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 16:57:55 +0530

    Done with console.log()

---
+++ consoleLog.js
@@ -0,0 +1,14 @@
+// console.log() method is the most commonly used console method.
+// It is used to output message in the console.
+// It can output all kinds of objets like string, number, boolean, array, HTML elements, etc.
+
+console.log(2706); // number
+console.log("Hello World"); // string
+console.log(25 === 25); // boolean
+const arr = [23, 89, 1234, 1, 629];
+console.log(arr); // array
+const obj = {
+  name: "Aditya",
+  age: 21,
+};
+console.log(obj); // Object
