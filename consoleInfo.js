0a26e7137412edbfc005a2b96ae0c2f2a97972ec
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 18:56:23 +0530

    Create consoleInfo.js

---
+++ consoleInfo.js
@@ -0,0 +1,13 @@
+// The console.info() method is used to display the important messages to the console.
+// In this method, the parameter can be of string, array, or objects.
+
+const str = "This is an information";
+const arr = ["Audi", "Mercedes", "BMW", "Lamborghini", "Ferrari", "Tesla"];
+const obj = {
+  name: "Aditya",
+  age: 1,
+};
+
+console.info(str);
+console.info(arr);
+console.info(obj);
