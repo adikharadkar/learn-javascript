81aa985b299f1ec94aed60efebb3e8f27f1cb43c
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 18:10:40 +0530

    Update consoleClear.js

---
+++ consoleClear.js
@@ -0,0 +1,6 @@
+// The console.clear() method clears the console if it is allowed by the environment
+
+const num = 12;
+console.log(num); // Prints 12 in the console
+console.clear(); // Clears the console
+console.log("Hello");
