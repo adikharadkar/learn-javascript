3f96af8063ae2319a6950a77a4c125fb5cd3e146
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 18:23:59 +0530

    Update consoleCount.js

---
+++ consoleCount.js
@@ -0,0 +1,8 @@
+// The console.count() method logs the number of time count() function has been called.
+// The count() method accepts an argument which is label to the output, it is logged every time count() function is called.
+// It is optional. Its default value is "default".
+
+for (let i = 0; i < 10; i++) {
+  console.count("Count Number");
+  console.log(i);
+}
