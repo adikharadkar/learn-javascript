566ba44fa1f786a5f72c4c282171f8aeb24ce9e8
adikharadkar <adityakharadkar27@gmail.com>
2022-02-25 18:07:11 +0530

    Update consoleAssert.js

---
+++ consoleAssert.js
@@ -0,0 +1,5 @@
+// console.assert() method asserts a condition and if the condition is false, then it outputs a message in the console.
+// If the condition is true then nothing happens.
+
+console.assert(25 == 25, "25 is equal to 25"); // Output will not print in the console as the condition is TRUE
+console.assert(25 === "25", "25 is not equal to '25'"); // Output will print in the console as the condition is FALSE
